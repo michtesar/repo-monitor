81d437dc33331fb50d007deb8da6aa1f7ac83b9a
Michael Tesař <michtesar@gmail.com>
2024-05-02 12:40:47 +0200

    Add statistics model

---
+++ repo_monitor/models/stats.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+
+
+class Statistics(BaseModel):
+    repository: str
+    average: float
+    std: float
+    min: float
+    max: float
+    first: datetime
+    last: datetime
+    n_events: int
+    timestamps: list[int]
+    inter_timestamps_intervals: list[int]
