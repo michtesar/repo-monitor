6a3981ab220142caf8ba84a81e81f8652b8fabf3
Michael Tesař <michtesar@gmail.com>
2024-05-02 11:13:26 +0200

    Add /events API scaffolding

--- repo_monitor/app.py
+++ repo_monitor/app.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
 
+from repo_monitor.models.repos import Repositories
+from repo_monitor.repository import Repository
+
 app = FastAPI()
 
 
 @app.get("/")
 async def root() -> dict[str, str]:
     return {"message": "Hello World"}
+
+
+@app.post("/events")
+async def get_events(repositories: Repositories) -> dict:
+    repos = [Repository(r) for r in repositories.repositories]
+    result = {}
+    for repo in repos:
+        result[repo.url] = repo.events
+    return {"results": result}
